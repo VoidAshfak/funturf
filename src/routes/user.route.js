01416a591d8201153d147f95394dcb24edb6b4c4
VoidAshfak <1703103@student.ruet.ac.bd>
2025-05-13 02:32:02 +0600

    migrate from mongoose to PRISMA

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { registerUser, loginUser, logoutUser, tokenRefresh } from "../controllers/user.controler.js";
 import {upload} from "../middlewares/multer.middleware.js";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verifyJWT, encryptPassword } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -12,6 +12,7 @@ router.route("/register").post(
             maxCount: 1
         }
     ]),
+    encryptPassword,
     registerUser
 );
 
