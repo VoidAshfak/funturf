00da86ff2b4a02ff51471ada04cae3df0a557d85
VoidAshfak <asfaqul.asif@gmail.com>
2025-10-01 01:45:14 +0600

    add reverse proxy server. setup postgresql for the project

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,10 @@
 import {app} from "./app.js";
 
+const PORT = process.env.PORT || 8080;
 
-app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log('up on', PORT));
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
